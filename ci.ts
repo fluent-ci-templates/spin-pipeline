2399477bead11d197714b6852cfacdab6c4a8de3
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-27 20:58:13 +0000

    bump version code

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   deploy,
-} from "https://pkg.fluentci.io/spin_pipeline@v0.8.1/mod.ts";
+} from "https://pkg.fluentci.io/spin_pipeline@v0.8.2/mod.ts";
 
 await build();
 await deploy();
